55a454dedcfe954159da358d938fa271376f04a2
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-01-05 16:54:11 +0000

    Add Code in CryptocurrencyTracker File

--- src/components/CryptocurrencyTracker/index.js
+++ src/components/CryptocurrencyTracker/index.js
@@ -1,1 +1,11 @@
-// Write your code here
+import './index.css'
+import CryptocurrenciesList from './components/CryptocurrenciesList'
+const CryptocurrencyTracker = () => {
+  return (
+    <div classname="bg-container">
+      <CryptocurrenciesList />
+    </div>
+  )
+}
+
+export default CryptocurrencyTracker
