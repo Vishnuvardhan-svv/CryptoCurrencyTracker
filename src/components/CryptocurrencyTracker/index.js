a97d3e9e585ee24b4f28ad2fb482e81c11492e1e
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-01-05 17:39:55 +0000

    Code Changes All Files

--- src/components/CryptocurrencyTracker/index.js
+++ src/components/CryptocurrencyTracker/index.js
@@ -1,12 +1,10 @@
 import './index.css'
 import CryptocurrenciesList from '../CryptocurrenciesList'
 
-const CryptocurrencyTracker = () => {
-  return (
-    <div className="bg-container">
-      <CryptocurrenciesList />
-    </div>
-  )
-}
+const CryptocurrencyTracker = () => (
+  <div className="bg-container">
+    <CryptocurrenciesList />
+  </div>
+)
 
 export default CryptocurrencyTracker
