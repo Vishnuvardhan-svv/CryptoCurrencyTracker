cc44f294e9a697b36f826b28a25761f2e5df7ca7
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-01-05 17:37:00 +0000

    Code Changes All Files

--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -1,11 +1,12 @@
 import './index.css'
+
 const CryptocurrencyItem = props => {
   const {eachItem} = props
   const {currencyName, currencyLogo, usdValue, euroValue} = eachItem
   return (
     <li>
       <div>
-        <img src={currencyLogo} />
+        <img src={currencyLogo} alt={currencyName} />
         <p>{currencyName}</p>
       </div>
       <div>
